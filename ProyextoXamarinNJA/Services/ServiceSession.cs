47b5117bd36ef5f62e1a4357c3682f50a51f9672
Naza <nazari_93@hotmail.com>
2021-06-04 00:14:48 +0200

    ServiceSession

---
+++ ProyextoXamarinNJA/Services/ServiceSession.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyextoXamarinNJA.Services
+{
+    public class ServiceSession
+    {
+        public int IdUsuario { get; set; }
+
+        public ServiceSession()
+        {
+            this.IdUsuario = 0;
+        }
+    }
+}
